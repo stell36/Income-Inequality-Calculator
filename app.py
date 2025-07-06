7187e02c879bb11a5f6911e282b41c70bd82d246
Stephen Elliott <112535251+stell36@users.noreply.github.com>
2025-07-05 22:38:54 -0400

    Add files via upload
    App.py has been updated so that the user can upload a one dimensional file with a list of incomes. The gini coefficient is then calculated from the list of incomes in a pairwise and cumulative fashion.

--- app.py
+++ app.py
@@ -1,30 +1,47 @@
-from dash import Dash, dcc, html, Input, Output, callback
+from dash import Dash, dcc, html, Input, Output, callback, State
+#import dash
+import pandas as pd
+import io
+import base64
+import gini_coefficient
 
 app = Dash()
 
 app.layout = html.Div([
-    html.H6("Change the value in the text box to see callbacks in action!"),
-    html.Div([
-        "Input: ",
-        dcc.Input(id='my-input', value='initial value', type='text')
-    ]),
+    html.H6("Upload a 1-column CSV file to see callbacks in action!"),
+    dcc.Upload(
+        id='upload-data',
+        children=html.Button('Upload CSV'),
+        multiple=False
+    ),
     html.Br(),
     html.Div(id='my-output'),
-
 ])
 
-
 @callback(
-    Output(component_id='my-output', component_property='children'),
-    Input(component_id='my-input', component_property='value')
+    Output('my-output', 'children'),
+    Input('upload-data', 'contents'),
+    State('upload-data', 'filename')
 )
-def update_output_div(input_value):
+def update_output_div(contents, filename):
+    if contents is None:
+        return "No file uploaded yet."
+    content_type, content_string = contents.split(',')
+    decoded = base64.b64decode(content_string)
     try:
-        float(input_value)
-    except ValueError:
-        return 'Please enter a valid number dork.'
-    return f'Output: {3 * float(input_value)}'
-
+        df = pd.read_csv(io.StringIO(decoded.decode('utf-8')), header=None)
+        if df.shape[1] != 1:
+            return "Please upload a CSV with exactly one column."
+        
+        result = df.iloc[:, 0].astype(float)
+        gini_array = [val for val in result if not pd.isna(val)]
+        return html.Div([
+            html.H6("Output:"),
+            html.H6(f"Gini Coefficient (Pairwise): {gini_coefficient.gini_pairwise(gini_array)}"),
+            html.H6(f"Gini Coefficient (Cumulative): {gini_coefficient.gini_cumulative(gini_array)}"),
+        ])
+    except Exception as e:
+        return f"Error processing file: {e}"
 
 if __name__ == '__main__':
     app.run(debug=True)
